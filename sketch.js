3bd9645def4cb2f2f6e1dd470d0d193e9d5033a0
unknown <k.panwar@share.epsb.ca>
2020-12-23 17:27:59 -0700

    added text

--- sketch.js
+++ sketch.js
@@ -40,6 +40,8 @@ function setup(){
 
 function draw(){
     background(backgroundImg);
+    textSize(25)
+    text("Angry Bird Game",200,100);
     Engine.update(engine);
     console.log(box2.body.position.x);
     console.log(box2.body.position.y);
